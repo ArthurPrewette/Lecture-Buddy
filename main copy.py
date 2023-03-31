674d901f12f54d50b63a30cd887e7d13c207ceba
ArthurPrewette <98502348+ArthurPrewette@users.noreply.github.com>
2023-03-31 11:32:50 -0400

    Update main copy.py
    Cut unused imports, simplified variable names, reorganized sections to flow more logically, and created further descriptions.

--- main copy.py
+++ main copy.py
@@ -1,23 +1,19 @@
-lectname = "hist_L19" #Update me every time.  
+lectname = "hist_L19"  #!!!                 Update me every time.
 lectlen = '39:23'                           #Exact lecture length if asynchronous. 
 speed =2.8                                  #This is your playback speed multiplier. 
-ss_region = (530,200,1000,750)              #Region of the lecture on your monitor, I use windowspy to check this.
-path = 'THIS FOLDER CONTAINING THIS FILE'   #update me once. 
-save_folder = 'UPDATE ME ONCE'              #update me once.
+path = 'THIS FOLDER CONTAINING THIS FILE'   #update me on first use. 
+save_folder = 'UPDATE ME ONCE'              #update me on first use.
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'#confirm/update me once.
-
-
-
-import pyautogui
+##########################################################################################################################################################
+# When you press run, put your cursor on the top left corner of the lecture area until prompted to move it to the bottom right corner of the lecture area. 
+##########################################################################################################################################################
 import time
-from PIL import Image
 import pytesseract
-import argparse
-from cv2 import cv2
 import os
 import SS_ORC as SS_OCR
 import errno
 from ctypes import windll, Structure, c_long, byref
+
 
 ss_path1 = path+"\\"+"check1.png"
 ss_path2 = path+"\\"+"check2.png"
@@ -28,52 +24,59 @@ except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 
+# This is to prevent overwriting a file when you forget to change the lecture name. 
+# If you forget, it will append to the historical file.
 filename = 'Screenshots\\%s\\transcript.txt' % lectname
 file_exists = os.path.isfile(filename)
 if file_exists:
-    pass
+    pass 
 else:
     new = open(filename, "w")
     new.close()
 
+# This section is to determing lecture area. 
+# I'm not going to write the code to automatically determine the size/location of a videoplayer. 
 class POINT(Structure):
     _fields_ = [("x", c_long), ("y", c_long)]
-
-def queryMousePosition():
+def MousePosition():
     pt = POINT()
     windll.user32.GetCursorPos(byref(pt))
     return (pt.x,pt.y)
-
+print("place your mouse on the top left corner of lecture window.")
 for i in range(5):
     count = 5-i
     time.sleep(1)
     print('Pos1:\t%is' % count)      
-pos1 = queryMousePosition()
+pos1 = MousePosition()
+print("place your mouse on the bottom right corner of lecture window.")
 for i in range(5):
     count = 5-i
     time.sleep(1)
-    print('Pos2:\t%is'% count)        
-pos2 = queryMousePosition()
-print(pos1)
-print(pos2)
+    print('Pos2:\t%is'% count)       
+pos2 = MousePosition()
+print("Done.\n Move your mouse away from lecture window.") 
 
+print(pos1)
+print(pos2) #printed so you can check with windowspy and restart before lecture starts if you messed it up. 
 regx2 = pos2[0]-pos1[0]
 regy2 = pos2[1]-pos1[1]
 ss_region = (pos1[0],pos1[1],regx2,regy2)
 
+
+#These are the characters we want to track. (we dont want to track noise from inaccurate OCR processing)
 charlistl = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 charlistu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 charcount1l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount1u = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount2l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount2u = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
 unknowna = []
 unknownb = []
-
 outcome = 0
 outloop = 0
 
+#Below is an example pattern of how the image referencing progresses throughout the script.
+#Where for [xn-yn], x is the reference image and y the comparison image. 
 #a1-b1, b1-a2, a2-b2, b2-a3, a3-b3
 run = 1 
 namea = 'check_a'
@@ -81,11 +84,13 @@ nameb = 'check_b'
 start_t = time.time()
 t = 0
 
+#Total Runtime.
 def update(s_time):
     curr = time.time()
     t = curr - s_time
     return t
 
+#accounting for speed multiplier effects. 
 def timer(lectlen,speed):
     lectlen = lectlen.split(':')
     second = int(lectlen[1]) / speed
@@ -93,6 +98,7 @@ def timer(lectlen,speed):
     length = minute + second
     return length
 
+#Removing illegal characters in file title. 
 def titler(text):
     tit = text.replace('\n','+')
     tit = tit.replace(" ","_")
@@ -112,35 +118,34 @@ def titler(text):
     tit = tit[:10]
     return tit
 
-length = timer(lectlen, speed) #calling SS_ORC
+length = timer(lectlen, speed)
 print(length)
 while t < length:
     t = update(start_t)
 
     if run == 1:
-#SCREENSHOT A 
+#Reference Screenshot
+#This is what is saved if it is determined that the comparison screenshot contains less data
         print('run 1: %i' % run)
         load_a = SS_OCR.Screenie('check_a',ss_region,path)  
         check_a = load_a.scshot()
         path_a = check_a[1]
         image_a = check_a[0]
-        
         text1 = load_a.imagetext()
-
         tc_load_a = SS_OCR.TextCheck(text1,charlistl,charlistu,charcount1l,charcount1u,unknownb)
         tc_a = tc_load_a.counting()
         tc_a = list(tc_a)
         count_a_l, count_a_u, len_a_l, len_a_u = tc_a
         #print(text1)
-
         words = tc_load_a.words()
         words_edit_a = words[0]
         words_orig_a = words[1]
-
-
         time.sleep(5)
-# SCREENSHOT B 
-
+        
+#Comparison Screenshot
+#This turns into reference screenshot if it is determined that no data was lost, i.e. both that:
+#1: Text was not swapped with other text (slide change) or erased (partial or full slide change). 
+#2: No handwriting was erased.
         load_b = SS_OCR.Screenie('check_b',ss_region,path)
         check_b = load_b.scshot()
         path_b = check_b[1]
@@ -164,7 +169,6 @@ while t < length:
     elif not run == 1 and not outloop == 3:
         
         print('run: %i' % run)
-
         path_a = check_b[1]
         image_a = check_b[0]
         text1 = text2
@@ -185,7 +189,6 @@ while t < length:
 
         text2 = load_b.imagetext()     
 
-        
         tc_load_b = SS_OCR.TextCheck(text2,charlistl,charlistu,charcount2l,charcount2u,unknownb)
         tc_b = tc_load_b.counting()
         tc_b = list(tc_b)
@@ -285,9 +288,8 @@ while t < length:
         st2 = outcome[3]
         prev = outcome[4]
         print('*666*: %ls\t %s\n\t%s\t%s' % (cause, prev, st1, st2))
-
     else:
-        print('watafak')
+        print('Something went HORRIBLY wrong.')
     run = run + 1
 
     print("%ls\n%ls" % (words_edit_a, words_orig_a))
