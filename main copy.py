e492b0e5e14d518bcc0ce078345c065feb63557e
ArthurPrewette <98502348+ArthurPrewette@users.noreply.github.com>
2023-03-31 12:23:50 -0400

    Update main copy.py
    Minor updates

--- main copy.py
+++ main copy.py
@@ -1,8 +1,8 @@
-lectname = "hist_L19"  #!!!                 Update me every time.
+lectname = "hist_L19" #Update me every time or i will append to this file. 
 lectlen = '39:23'                           #Exact lecture length if asynchronous. 
 speed =2.8                                  #This is your playback speed multiplier. 
-path = 'THIS FOLDER CONTAINING THIS FILE'   #update me on first use. 
-save_folder = 'UPDATE ME ONCE'              #update me on first use.
+path = 'THIS FOLDER CONTAINING THIS FILE'   #update me once. 
+save_folder = 'UPDATE ME ONCE'              #update me once.
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'#confirm/update me once.
 ##########################################################################################################################################################
 # When you press run, put your cursor on the top left corner of the lecture area until prompted to move it to the bottom right corner of the lecture area. 
@@ -13,7 +13,6 @@ import os
 import SS_ORC as SS_OCR
 import errno
 from ctypes import windll, Structure, c_long, byref
-
 
 ss_path1 = path+"\\"+"check1.png"
 ss_path2 = path+"\\"+"check2.png"
@@ -57,26 +56,26 @@ pos2 = MousePosition()
 print("Done.\n Move your mouse away from lecture window.") 
 
 print(pos1)
-print(pos2) #printed so you can check with windowspy and restart before lecture starts if you messed it up. 
+print(pos2) #printed so you can check with windowspy and restart before lecture starts. 
 regx2 = pos2[0]-pos1[0]
 regy2 = pos2[1]-pos1[1]
 ss_region = (pos1[0],pos1[1],regx2,regy2)
 
 
-#These are the characters we want to track. (we dont want to track noise from inaccurate OCR processing)
+#These are the characters we want to track. (we dont want to track noise from handwriting)
 charlistl = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 charlistu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 charcount1l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount1u = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount2l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 charcount2u = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 unknowna = []
 unknownb = []
+
 outcome = 0
 outloop = 0
 
-#Below is an example pattern of how the image referencing progresses throughout the script.
-#Where for [xn-yn], x is the reference image and y the comparison image. 
 #a1-b1, b1-a2, a2-b2, b2-a3, a3-b3
 run = 1 
 namea = 'check_a'
@@ -84,13 +83,11 @@ nameb = 'check_b'
 start_t = time.time()
 t = 0
 
-#Total Runtime.
 def update(s_time):
     curr = time.time()
     t = curr - s_time
     return t
 
-#accounting for speed multiplier effects. 
 def timer(lectlen,speed):
     lectlen = lectlen.split(':')
     second = int(lectlen[1]) / speed
@@ -98,7 +95,6 @@ def timer(lectlen,speed):
     length = minute + second
     return length
 
-#Removing illegal characters in file title. 
 def titler(text):
     tit = text.replace('\n','+')
     tit = tit.replace(" ","_")
@@ -120,32 +116,34 @@ def titler(text):
 
 length = timer(lectlen, speed)
 print(length)
+
 while t < length:
     t = update(start_t)
 
     if run == 1:
-#Reference Screenshot
-#This is what is saved if it is determined that the comparison screenshot contains less data
+#SCREENSHOT A 
         print('run 1: %i' % run)
         load_a = SS_OCR.Screenie('check_a',ss_region,path)  
         check_a = load_a.scshot()
         path_a = check_a[1]
         image_a = check_a[0]
+        
         text1 = load_a.imagetext()
+
         tc_load_a = SS_OCR.TextCheck(text1,charlistl,charlistu,charcount1l,charcount1u,unknownb)
         tc_a = tc_load_a.counting()
         tc_a = list(tc_a)
         count_a_l, count_a_u, len_a_l, len_a_u = tc_a
         #print(text1)
+
         words = tc_load_a.words()
         words_edit_a = words[0]
         words_orig_a = words[1]
+
+
         time.sleep(5)
-        
-#Comparison Screenshot
-#This turns into reference screenshot if it is determined that no data was lost, i.e. both that:
-#1: Text was not swapped with other text (slide change) or erased (partial or full slide change). 
-#2: No handwriting was erased.
+#SCREENSHOT B 
+
         load_b = SS_OCR.Screenie('check_b',ss_region,path)
         check_b = load_b.scshot()
         path_b = check_b[1]
@@ -169,6 +167,7 @@ while t < length:
     elif not run == 1 and not outloop == 3:
         
         print('run: %i' % run)
+
         path_a = check_b[1]
         image_a = check_b[0]
         text1 = text2
@@ -189,6 +188,7 @@ while t < length:
 
         text2 = load_b.imagetext()     
 
+        
         tc_load_b = SS_OCR.TextCheck(text2,charlistl,charlistu,charcount2l,charcount2u,unknownb)
         tc_b = tc_load_b.counting()
         tc_b = list(tc_b)
@@ -281,15 +281,16 @@ while t < length:
         prev = outcome[4]
         print('*3*: %ls\t %s\n\t%s\t%s' % (cause, prev, st1, st2))
         os.remove(path_b)
-    elif outcome[0] == 666:
+    elif outcome[0] == 4:
         print('logic is a construct')
         cause = outcome[1]
         st1 = outcome[2]
         st2 = outcome[3]
         prev = outcome[4]
-        print('*666*: %ls\t %s\n\t%s\t%s' % (cause, prev, st1, st2))
+        print('*4*: %ls\t %s\n\t%s\t%s' % (cause, prev, st1, st2))
+
     else:
-        print('Something went HORRIBLY wrong.')
+        print('watafak')
     run = run + 1
 
     print("%ls\n%ls" % (words_edit_a, words_orig_a))
